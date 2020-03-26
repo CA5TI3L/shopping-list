a0a0c67bea3811416c6626707866ffb83989c26c
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-03-26 15:32:03 +0200

    Build shopping-list-item component

--- src/app/components/shopping-list-item/shopping-list-item.component.ts
+++ src/app/components/shopping-list-item/shopping-list-item.component.ts
@@ -1,13 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, ChangeDetectionStrategy } from '@angular/core';
+import { ShoppingListItem } from '../../models/shopping';
+import { EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-shopping-list-item',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './shopping-list-item.component.html',
   styleUrls: ['./shopping-list-item.component.css']
 })
 export class ShoppingListItemComponent implements OnInit {
 
+  @Input() shoppingListItem: ShoppingListItem;
+  @Output() checked = new EventEmitter<boolean>();
+  @Output() removed = new EventEmitter();
+
   constructor() { }
+
+  checkedChanged(event) {
+    this.checked.emit(event.checked);
+  }
+
+  removeMe() {
+    this.removed.emit();
+  }
 
   ngOnInit(): void {
   }
