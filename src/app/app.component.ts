2aa5be2b47d9cf0ca19775c62169e925a7eb50f6
FRossouw <60608743+FRossouw@users.noreply.github.com>
2020-03-27 11:22:32 +0200

    Feature app component create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ShoppingListItem } from './models/shopping';
+import { ShoppingState, selectListItems } from './store/reducers/shopping.reducer';
+import { Store, select } from '@ngrx/store';
+import { updateListItem, removeListItem, addListItem, getListItems } from './store/actions/shopping.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'shopping-list';
+export class AppComponent implements OnInit {
+
+  newItemName = '';
+  newItemId = 0;
+  shoppingListItems$: Observable<ShoppingListItem[]>;
+
+  constructor(private shoppingListStore: Store<ShoppingState>) { }
+  
+  ngOnInit(): void {
+    this.shoppingListItems$ = this.shoppingListStore.pipe(
+      select(selectListItems)
+    );
+    this.shoppingListStore.dispatch(getListItems());
+  }
+
+  onCheckChanged(event: boolean, eventItem: ShoppingListItem) {
+    //item.completed = event;
+    this.shoppingListStore.dispatch(updateListItem({ item: {...eventItem, completed: event} }));
+  }
+
+  onRemoved(item: ShoppingListItem) {
+    this.shoppingListStore.dispatch(removeListItem({ item }));
+  }
+
+  addNewItem() {
+    this.shoppingListStore.dispatch(addListItem({ item: { id: this.newItemId, label: this.newItemName, completed: false } }));
+    this.newItemName = '';
+    this.newItemId = 0;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { StoreModule } from '@ngrx/store';
+import { ActionReducer, StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { reducers } from './store';
+import { ShoppingEffects } from './store/effects/shopping.effects';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
 import { ShoppingListItemComponent } from './components/shopping-list-item/shopping-list-item.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule, MatFormField } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+
+// Meta-reducers
+// console.log all actions
+export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log('state', state);
+    console.log('action', action);
+    return reducer(state, action);
+  };
+}
 
 @NgModule({
   declarations: [
@@ -17,9 +38,19 @@ import { ShoppingListItemComponent } from './components/shopping-list-item/shopp
   imports: [
     BrowserModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    StoreModule.forRoot({}, {}),
+    StoreModule.forRoot(reducers, {metaReducers: [debug]}),
     EffectsModule.forRoot([]),
-    BrowserAnimationsModule
+    EffectsModule.forFeature([ShoppingEffects]),
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
